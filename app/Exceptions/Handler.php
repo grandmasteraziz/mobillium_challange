132004fa35b038847e7c3083ddbbff6cc7b646b3
grandmasteraziz <grandmasteraziz@hotmail.com>
2019-01-17 13:50:03 +0300

    edited exception handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,18 +4,28 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
+use App\Traits\ApiResponser;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
 
 class Handler extends ExceptionHandler
 {
+
     use ApiResponser;
+
     /**
-     * A list of the exception types that should not be reported.
+     * A list of the exception types that are not reported.
      *
      * @var array
      */
     protected $dontReport = [
         //
     ];
+
     /**
      * A list of the inputs that are never flashed for validation exceptions.
      *
@@ -25,10 +35,9 @@ class Handler extends ExceptionHandler
         'password',
         'password_confirmation',
     ];
+
     /**
      * Report or log an exception.
-     *
-     * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
      *
      * @param  \Exception  $exception
      * @return void
@@ -37,6 +46,7 @@ class Handler extends ExceptionHandler
     {
         parent::report($exception);
     }
+
     /**
      * Render an exception into an HTTP response.
      *
@@ -46,79 +56,43 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        $response = $this->handleException($request, $exception);
-        app(CorsService::class)->addActualRequestHeaders($response, $request);
-        return $response;
+        
+        if ($request->expectsJson()){
+            $response = $this->handleException($exception, $request);
+            return $response;
+        }
+
+        return parent::render($request, $exception);
     }
-    public function handleException($request, Exception $exception)
+
+
+     
+
+    
+    public function handleException( Exception $exception,$request)
     {
         if ($exception instanceof ValidationException) {
-            return $this->convertValidationExceptionToResponse($exception, $request);
+            // calling apiresponser trait
+            return $this->errorResponse($exception->validator->errors()->getMessages(),422); 
         }
+
+
         if ($exception instanceof ModelNotFoundException) {
             $modelName = strtolower(class_basename($exception->getModel()));
-            return $this->errorResponse("Does not exists any {$modelName} with the specified identificator", 404);
+            return $this->convertValidationExceptionToResponse("Does not exists any {$modelName} with the specified identificator", $request , 422);
         }
-        if ($exception instanceof AuthenticationException) {
-            return $this->unauthenticated($request, $exception);
-        }
-        if ($exception instanceof AuthorizationException) {
-            return $this->errorResponse($exception->getMessage(), 403);
-        }
+        
         if ($exception instanceof MethodNotAllowedHttpException) {
             return $this->errorResponse('The specified method for the request is invalid', 405);
         }
         if ($exception instanceof NotFoundHttpException) {
+           
             return $this->errorResponse('The specified URL cannot be found', 404);
         }
-        if ($exception instanceof HttpException) {
-            return $this->errorResponse($exception->getMessage(), $exception->getStatusCode());
-        }
-        if ($exception instanceof QueryException) {
-            $errorCode = $exception->errorInfo[1];
-            if ($errorCode == 1451) {
-                return $this->errorResponse('Cannot remove this resource permanently. It is related with any other resource', 409);
-            }
-        }
-        if ($exception instanceof TokenMismatchException) {
-            return redirect()->back()->withInput($request->input());
-        }
-        if (config('app.debug')) {
-            return parent::render($request, $exception);            
-        }
-        return $this->errorResponse('Unexpected Exception. Try later', 500);
+
     }
-    /**
-     * Convert an authentication exception into an unauthenticated response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Illuminate\Auth\AuthenticationException  $exception
-     * @return \Illuminate\Http\Response
-     */
-    protected function unauthenticated($request, AuthenticationException $exception)
-    {
-        if ($this->isFrontend($request)) {
-            return redirect()->guest('login');
-        }
-        return $this->errorResponse('Unauthenticated.', 401);
-    }
-    /**
-     * Create a response object from the given validation exception.
-     *
-     * @param  \Illuminate\Validation\ValidationException  $e
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    protected function convertValidationExceptionToResponse(ValidationException $e, $request)
-    {
-        $errors = $e->validator->errors()->getMessages();
-        if ($this->isFrontend($request)) {
-            return $request->ajax() ? response()->json($error, 422) : redirect()
-                ->back()
-                ->withInput($request->input())
-                ->withErrors($errors);
-        }
-        
-        return $this->errorResponse($errors, 422);
-    }
+
+
+   
+
 }
--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,13 @@ Route::middleware('auth:api')->get('api/userto', function (Request $request) {
     return json_encode(array ('a'=>1,'b'=>2,'c'=>3,'d'=>4,'e'=>5));
 });
 
-Route::get('/api/me',function(){
+Route::post('/api/me',function(Request $request){
+    $request->validate([
+        'title' => 'required|unique:posts|max:255',
+        'body' => 'required',
+    ]);
+
+
     return json_encode(array ('a'=>1,'b'=>2,'c'=>3,'d'=>4,'e'=>5));
 });
 
